7e4dc9296f43b40dbf07a471296331d1fa3a3974
Kenzaii <45958062+Kenzaii@users.noreply.github.com>
2025-03-25 15:02:52 +0800

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Airtable Configuration
-window.AIRTABLE_URL = 'https://api.airtable.com/v0';
-window.BASE_ID = 'appipp8LFUGElp3Di';
-window.API_KEY = 'pathhm43xpa5TmwaG.3b6a259e92f40f97170ba1680a4db0af5c767076db223aef553ae844cf94e542'; 
